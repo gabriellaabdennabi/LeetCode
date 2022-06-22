1781e5ee9fce94de9559a073a366a67e1995e73f
Gabi Abdennabi <gabde002@fiu.edu>
2022-06-22 11:33:08 -0400

    Time: 57 ms (70.07%), Space: 13.9 MB (82.35%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.py
+++ 162-find-peak-element/162-find-peak-element.py
@@ -1,24 +1,19 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
         
-        '''
-        [1,2,3,1]
-         
-        peak element is greater than its neighbors 
-        binary search problem 
         
+        left = 0 
+        right = len(nums) -1 
         
-        '''
-        if len(nums) == 1:
-            return 0 
-        
-        for i in range(len(nums)-1):
-            if i == 0 and nums[i] > nums[i+1]:
-                return i
-            
-            elif nums[i] > nums[i-1] and nums[i] > nums[i+1]:
-                return i
+        while left < right:
+            mid = (left+right) // 2
             
             
-        return len(nums) -1 
+            if nums[mid] > nums[mid+1]:
+                right = mid 
+            
+            else:
+                left = mid + 1
+                
+        return left
         
